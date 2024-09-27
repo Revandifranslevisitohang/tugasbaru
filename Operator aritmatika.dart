09bf4112f73c9c38c2af723767c34a8941862f9c
Revandifranslevisitohang <mrrsgaming45@gmail.com>
2024-09-27 18:32:27 +0700

    Add files via upload

---
+++ Operator aritmatika.dart
@@ -0,0 +1,12 @@
+void main() {
+  var result1 = 10 + 10;     // 20
+  var result2 = 100 - 10;    // 90
+  var result3 = -5;          // -5
+  var result4 = 5;           // 5
+  var result5 = 5 * 5;       // 25
+  var result6 = 10 / 3;      // 3.3333...
+  var result7 = 10 ~/ 3;     // 3
+  var result8 = 10 % 3;      // 1
+
+  print('$result1, $result2, $result3, $result4, $result5, $result6, $result7, $result8');
+} 
